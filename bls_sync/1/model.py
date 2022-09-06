929e90058a57a27d1b729210dd6dc59af459c830
Sazzad <sazzadhrz@gmail.com>
2022-09-06 21:12:51 +0600

    Update model.py

--- bls_sync/1/model.py
+++ bls_sync/1/model.py
@@ -103,61 +103,61 @@ class TritonPythonModel:
 #         return responses
     
     
-        def execute(self, requests):
-            """`execute` must be implemented in every Python model. `execute`
-            function receives a list of pb_utils.InferenceRequest as the only
-            argument. This function is called when an inference request is made
-            for this model. Depending on the batching configuration (e.g. Dynamic
-            Batching) used, `requests` may contain multiple requests. Every
-            Python model, must create one pb_utils.InferenceResponse for every
-            pb_utils.InferenceRequest in `requests`. If there is an error, you can
-            set the error argument when creating a pb_utils.InferenceResponse
-            Parameters
-            ----------
-            requests : list
-            A list of pb_utils.InferenceRequest
-            Returns
-            -------
-            list
-            A list of pb_utils.InferenceResponse. The length of this list must
-            be the same as `requests`
-            """
+    def execute(self, requests):
+        """`execute` must be implemented in every Python model. `execute`
+        function receives a list of pb_utils.InferenceRequest as the only
+        argument. This function is called when an inference request is made
+        for this model. Depending on the batching configuration (e.g. Dynamic
+        Batching) used, `requests` may contain multiple requests. Every
+        Python model, must create one pb_utils.InferenceResponse for every
+        pb_utils.InferenceRequest in `requests`. If there is an error, you can
+        set the error argument when creating a pb_utils.InferenceResponse
+        Parameters
+        ----------
+        requests : list
+        A list of pb_utils.InferenceRequest
+        Returns
+        -------
+        list
+        A list of pb_utils.InferenceResponse. The length of this list must
+        be the same as `requests`
+        """
 
-            responses = []
-            # Every Python backend must iterate over everyone of the requests
-            # and create a pb_utils.InferenceResponse for each of them.
-            for request in requests:
-                # Get INPUT0
-                in_0 = pb_utils.get_input_tensor_by_name(request, "INPUT0")
+        responses = []
+        # Every Python backend must iterate over everyone of the requests
+        # and create a pb_utils.InferenceResponse for each of them.
+        for request in requests:
+            # Get INPUT0
+            in_0 = pb_utils.get_input_tensor_by_name(request, "INPUT0")
 
-                # # Get INPUT1
-                # in_1 = pb_utils.get_input_tensor_by_name(request, "INPUT1")
+            # # Get INPUT1
+            # in_1 = pb_utils.get_input_tensor_by_name(request, "INPUT1")
 
-                # Get Model Name
-                model_name = pb_utils.get_input_tensor_by_name(
-                    request, "MODEL_NAME")
+            # Get Model Name
+            model_name = pb_utils.get_input_tensor_by_name(
+                request, "MODEL_NAME")
 
-                # Model Name string
-                model_name_string = model_name.as_numpy()[0]
+            # Model Name string
+            model_name_string = model_name.as_numpy()[0]
 
-                # Create inference request object
-                infer_request = pb_utils.InferenceRequest(
-                    model_name=model_name_string,
-                    # requested_output_names=["OUTPUT0", "OUTPUT1"],
-                    requested_output_names=["OUTPUT0"],
-                    inputs=[in_0])
+            # Create inference request object
+            infer_request = pb_utils.InferenceRequest(
+                model_name=model_name_string,
+                # requested_output_names=["OUTPUT0", "OUTPUT1"],
+                requested_output_names=["OUTPUT0"],
+                inputs=[in_0])
 
-                infer_response = infer_request.exec()
+            infer_response = infer_request.exec()
 
-                if infer_response.has_error():
-                    raise pb_utils.TritonModelException(
-                        infer_response.error().message())
+            if infer_response.has_error():
+                raise pb_utils.TritonModelException(
+                    infer_response.error().message())
 
-                inference_response = pb_utils.InferenceResponse(
-                    output_tensors=infer_response.output_tensors())
-                responses.append(inference_response)
+            inference_response = pb_utils.InferenceResponse(
+                output_tensors=infer_response.output_tensors())
+            responses.append(inference_response)
 
-            return responses
+        return responses
 
     def finalize(self):
         """`finalize` is called only once when the model is being unloaded.
